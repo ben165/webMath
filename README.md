5e472c933b9ffcf47ef4c455e21bee231b609d88
pec35 <jakob.sigmund@gmail.com>
2023-09-28 11:55:32 +0200

    basic 3d plot und mathml

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Install following Python packages on the command line:
 
 ```
-apt-get install python3-flask python3-numpy python3-matplotlib
+apt-get install python3-flask python3-numpy python3-matplotlib python3-sympy
 ```
 
 Change inside this folder and start
--- helper.py
+++ helper.py
@@ -25,29 +25,29 @@ html {
 TAIL = "</body></html"
 
 hashTable = {
- "jakob": "rP+P6LOUG8iDWLZ44L9P10Psj",
- "benjamin": "SpNJrACJAFUHsEe7Q57tAteMV"
+    "jakob": "rP+P6LOUG8iDWLZ44L9P10Psj",
+    "benjamin": "SpNJrACJAFUHsEe7Q57tAteMV"
 }
 
 pwTable = {
- "jakob": "03c2d2d6ac2b18bef970ed2b9d26a42cc842d9e1c3d61f03060dd76e137bc8b0",
- "benjamin": "2ec8de72b6068a82cea6c2a0b3675fbc6ebf475fc6e892cce8aa7d9a9d9d2d51"
+    "jakob": "03c2d2d6ac2b18bef970ed2b9d26a42cc842d9e1c3d61f03060dd76e137bc8b0",
+    "benjamin": "2ec8de72b6068a82cea6c2a0b3675fbc6ebf475fc6e892cce8aa7d9a9d9d2d51"
 }
 
+
 def createHash(str1, user):
- try:
-  hashTable[user]
- except:
-  return "0000000000000000000000000000000" # value cant be created. Login will fail.
- str1 += hashTable[user]
- m = hashlib.sha256()
- m.update(str1.encode('utf-8'))
- return m.hexdigest()
+    try:
+        hashTable[user]
+    except:
+        return "0000000000000000000000000000000"  # value cant be created. Login will fail.
+    str1 += hashTable[user]
+    m = hashlib.sha256()
+    m.update(str1.encode('utf-8'))
+    return m.hexdigest()
 
 
 def getPw(user):
- try:
-  return pwTable[user]
- except:
-  return "0000000000000000000000000000001" # value cant be created. Login will fail.
-
+    try:
+        return pwTable[user]
+    except:
+        return "0000000000000000000000000000001"  # value cant be created. Login will fail.
